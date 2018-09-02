e1cc6e1d930f1b5d368e65d0e5a46bd5c9878608
artemsky <mr.artemsky@gmail.com>
2018-09-02 22:47:06 +0300

    #11 add image uploader

--- resources/assets/js/admin/Api/Image.js
+++ resources/assets/js/admin/Api/Image.js
@@ -2,7 +2,14 @@ import '../core';
 import Router from 'common/Router';
 
 export default class {
-  static store() {
-    return $.post(Router.name('admin.image.store'));
+  static store(file) {
+    const data = new FormData();
+    data.append('image', file);
+    return $.ajax(Router.name('admin.image.store'), {
+      method: 'post',
+      processData: false,
+      contentType: false,
+      data,
+    }).then(response => response.path);
   }
 }
