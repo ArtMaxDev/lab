ff9b9af8ecc24d5ddc5d1a055a5e97ca44fef986
ODDsama <bad.odd.sama@gmail.co>
2018-09-07 02:17:15 +0300

    #8 fix logging and add auth

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('admin.pages.login');
+    }
+
+    public function login(LoginRequest $request)
+    {
+        Auth::attempt($request->only(['email', 'password']));
+
+        return redirect()->route('admin.users.index');
+    }
+}
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -10,20 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/login', 'AuthController@showLoginForm')->name('users.login');
+Route::post('/login', 'AuthController@login')->name('users.login.post');
+Route::middleware(['auth', 'role:root'])->group(function () {
+    Route::get('/', function () {
+        return redirect()->route('admin.users.index');
+    })->name('index');
+    Route::view('/users', 'admin.pages.users')->name('users.index');
+    Route::view('/feedback', 'admin.pages.feedback')->name('feedback.index');
 
-Route::get('/', function () {
-    return redirect()->route('admin.users.index');
-})->name('admin.index');
-Route::view('/users', 'admin.pages.users')->name('admin.users.index');
-Route::view('/feedback', 'admin.pages.feedback')->name('admin.feedback.index');
+    Route::get('/publications', 'PublicationController@index')->name('publications.index');
+    Route::get('/publications/create', 'PublicationController@create')->name('publications.create');
+    Route::get('/publications/{publication}', 'PublicationController@edit')->name('publications.edit');
+    Route::post('/publications', 'PublicationController@store')->name('publications.store');
+    Route::put('/publications/{publication}', 'PublicationController@update')->name('publications.update');
 
-Route::get('/publications', 'PublicationController@index')->name('admin.publications.index');
-Route::get('/publications/create', 'PublicationController@create')->name('admin.publications.create');
-Route::get('/publications/{publication}', 'PublicationController@edit')->name('admin.publications.edit');
-Route::post('/publications', 'PublicationController@store')->name('admin.publications.store');
-Route::put('/publications/{publication}', 'PublicationController@update')->name('admin.publications.update');
-
-Route::view('/settings', 'admin.pages.settings')->name('admin.settings.index');
-Route::view('/login', 'admin.pages.login')->name('admin.users.login');
-Route::view('/sliders', 'admin.pages.sliders')->name('admin.sliders.index');
-Route::post('/image', 'ImageController')->name('image.upload');
+    Route::view('/settings', 'admin.pages.settings')->name('settings.index');
+    Route::view('/sliders', 'admin.pages.sliders')->name('sliders.index');
+    Route::post('/image', 'ImageController')->name('image.upload');
+});
