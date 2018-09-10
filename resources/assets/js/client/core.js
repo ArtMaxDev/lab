45f35d1fce9c50303e6dfec41b22118c6a2ee834
artemsky <mr.artemsky@gmail.com>
2018-09-10 21:32:17 +0300

    add fonts

--- resources/assets/js/client/core.js
+++ resources/assets/js/client/core.js
@@ -1,15 +1,25 @@
-import 'bootstrap';
+import fontLoader from 'common/fontLoader';
 
 const token = $('meta[name="csrf-token"]');
 
+fontLoader([
+  {
+    name: 'Roboto Condensed',
+    weight: [300, 400, 700]
+  },
+  {
+    name: 'Roboto Slab',
+    weight: [400, 700]
+  },
+]);
+
 if (token) {
-  $.ajaxSetup({
-    headers: {
-      'X-Requested-With': 'XMLHttpRequest',
-      'X-CSRF-TOKEN': token.attr('content')
-    }
-  });
+  // $.ajaxSetup({
+  //   headers: {
+  //     'X-Requested-With': 'XMLHttpRequest',
+  //     'X-CSRF-TOKEN': token.attr('content')
+  //   }
+  // });
 } else {
   console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-
--- resources/views/client/pages/index.blade.php
+++ resources/views/client/pages/index.blade.php
@@ -1,9 +1,39 @@
 @extends('client.master')
 @section('content')
-    <div class="container text-center mt-5">
-        <h1>Welcome to {{config('app.name')}}</h1>
-        <a href="{{route('admin.index')}}" class="btn btn-outline-dark">Go to Dashboard</a>
+    <div class="container">
+        <nav class="navbar">
+        <span id="navbar-toggle">
+            <i class="fas fa-bars"></i>
+        </span>
+            <a href="#" id="logo">ОНКОТЕРАНОСТИКА</a>
+            <ul id="main-nav">
+                <li>
+                    <a href="#" class="nav-links">Про нас</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-links">Послуги</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-links">Корисна Информация</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-links">Команда</a>
+                </li>
+                <li>
+                    <a href="#" class="nav-links">Контакти</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.row -->
     </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            Slider
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
 @endsection
 @push('scripts')
     <script defer src="{{mix('/assets/pages/index.js', 'client')}}"></script>
