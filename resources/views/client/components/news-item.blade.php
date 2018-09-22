5c3c8f4f8cb0504a88d0acccdddd6b79da9b2179
Maxim Vaschenko <bad.odd.sama@gmail.com>
2018-09-22 15:57:56 +0300

    some fixes

--- resources/views/client/components/news-item.blade.php
+++ resources/views/client/components/news-item.blade.php
@@ -1,6 +1,6 @@
 <a href="#" class="item news-item">
     <article class="content">
-        <img src="{{$news->image}}0" alt="" class="img" width="100%" height="230px">
+        <img src="{{$news->image}}" alt="" class="img" width="100%" height="230px">
         <time>{{$news->created_at}}</time>
         <h5 class="title">{{$news->title}}</h5>
         <p class="description">{{$news->description}}</p>
