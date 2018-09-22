ff03fc20b292b20b63166c33033519f948517916
Maxim Vaschenko <bad.odd.sama@gmail.com>
2018-09-22 20:44:20 +0300

    multilang

--- app/Http/Controllers/Client/MainController.php
+++ app/Http/Controllers/Client/MainController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers\Client;
 
 use App\Models\Publication;
 use App\Http\Controllers\Controller;
+use App\Models\Slide;
 
 class MainController extends Controller
 {
-    public function __invoke(Publication $publication)
+    public function __invoke(Publication $publication, Slide $slide)
     {
         return view('client.pages.index')->with([
             'news' => $publication->all(),
+            'slides' => $slide->all(),
         ]);
     }
 }
