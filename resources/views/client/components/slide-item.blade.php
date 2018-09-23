70f848638844319d4cbd0e951bc9bce17dade0f2
artemsky <mr.artemsky@gmail.com>
2018-09-23 10:17:05 +0300

    add image alts

--- resources/views/client/components/slide-item.blade.php
+++ resources/views/client/components/slide-item.blade.php
@@ -1,11 +1,11 @@
 <div class="slide">
-    <img class="image tns-lazy-img" data-src="/client/assets/images/static/slide1.jpg" alt="Slide 1">
+    <img class="image tns-lazy-img" data-src="{{$slide->image}}" alt="{{$slide->image_alt}}">
     <div class="content">
         <div class="container">
             <div class="content-right">
                 <h3 class="title">{{$slide->title}}</h3>
                 <p class="description">{{$slide->description}}</p>
-                <a href="#" class="btn">Узнать подробнее</a>
+                <a href="{{$slide->link}}" class="btn">Узнать подробнее</a>
             </div>
         </div>
     </div>
--- resources/views/client/pages/index.blade.php
+++ resources/views/client/pages/index.blade.php
@@ -10,20 +10,20 @@
     <section class="container section" id="services">
         <div class="services">
             <figure class="item">
-                <img src="/client/assets/images/static/service-1.png" alt="" class="img">
+                <img src="/client/assets/images/static/service-1.png" alt="Індивідуальна програма ранньої діагностики" class="img">
                 <figcaption>
                     <strong class="main-text">Індивідуальна програма ранньої діагностики</strong>
                     <p class="sub-text">Попереджений - означає озброєний!</p>
                 </figcaption>
             </figure>
             <figure class="item">
-                <img src="/client/assets/images/static/service-2.png" alt="" class="img">
+                <img src="/client/assets/images/static/service-2.png" alt="Молекулярно-епігенетична оцінка стану організму" class="img">
                 <figcaption>
                     <strong class="main-text">Молекулярно-епігенетична оцінка стану організму</strong>
                 </figcaption>
             </figure>
             <figure class="item">
-                <img src="/client/assets/images/static/service-3.png" alt="" class="img">
+                <img src="/client/assets/images/static/service-3.png" alt="Інноваційні діагностичні підходи" class="img">
                 <figcaption>
                     <strong class="main-text">Інноваційні діагностичні підходи</strong>
                     <p class="sub-text">Малоінвазивні методи дослідження</p>
@@ -38,7 +38,7 @@
         <div class="container">
             <h2 class="about-title">Клініка персоналізованого дизайну <br/> діагностики і терапії «Онкотераностика»</h2>
             <article class="article">
-                <img src="/client/assets/images/static/about.jpg" alt="" class="img">
+                <img src="/client/assets/images/static/about.jpg" alt="Клініка персоналізованого дизайну" class="img">
                 <div class="content">
                     <h3 class="title">Персоналізована медицина – це вже не забаганка, а нагальна необхідність сьогодення!</h3>
                     <p>Ми надаємо унікальні в Українi послуги з використанням лабораторної, епігенетичної та молекулярно-генетичної діагностики:
@@ -63,27 +63,27 @@
         <ul class="service-list">
             <li class="item">
                 <a href="#" class="link">
-                    <img class="img" src="/client/assets/images/static/s1.png" alt="">
+                    <img class="img" src="/client/assets/images/static/s1.png" alt="Рання діагностика раку">
                     <strong>Рання діагностика раку</strong>
                     <span class="sub-text">MiR-TECT<br/> ОНКО Check-Up</span>
                 </a>
             </li>
             <li class="item">
                 <a href="#" class="link">
-                    <img class="img" src="/client/assets/images/static/s2.png" alt="">
+                    <img class="img" src="/client/assets/images/static/s2.png" alt="Індивідуальна чутливість до протипухлинних препаратів ">
                     <strong>Індивідуальна чутливість до протипухлинних препаратів </strong>
                     <span class="sub-text">ОНКО Drug Test</span>
                 </a>
             </li>
             <li class="item">
                 <a href="#" class="link">
-                    <img class="img" src="/client/assets/images/static/s3.png" alt="">
+                    <img class="img" src="/client/assets/images/static/s3.png" alt="Онкогематологічні дослідження">
                     <strong>Онкогематологічні дослідження</strong>
                 </a>
             </li>
             <li class="item">
                 <a href="#" class="link">
-                    <img class="img" src="/client/assets/images/static/s4.png" alt="">
+                    <img class="img" src="/client/assets/images/static/s4.png" alt="Загальноклінічні аналізи">
                     <strong>Загальноклінічні аналізи</strong>
                 </a>
             </li>
@@ -106,7 +106,6 @@
     <section class="section container news" id="news">
         <h2 class="section-title">Новини</h2>
         <div id="slider-news">
-
             @each('client.components.news-item', $news, 'news')
         </div>
         <!-- /#slider-team -->
