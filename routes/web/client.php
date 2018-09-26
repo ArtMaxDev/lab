2a96738138b7d445a8dd43a57cd623a98b1db4a4
Maxim Vaschenko <bad.odd.sama@gmail.com>
2018-09-26 08:30:59 +0300

    fix feedback route

--- routes/web/client.php
+++ routes/web/client.php
@@ -25,5 +25,6 @@ Route::group([
     Route::get('/services/', 'BaseController@services')->name('client.services.index');
     Route::get('/services-2/', 'BaseController@services2')->name('client.services-2.index');
     Route::get('/services-3/', 'BaseController@services3')->name('client.services-3.index');
-    Route::post('/feedback', 'FeedbackController')->name('client.feedback.store');
 });
+
+Route::post('/feedback', 'FeedbackController')->name('client.feedback.store');
