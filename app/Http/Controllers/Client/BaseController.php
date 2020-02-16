243bbe3f06ba9d31e8329cff750a8a50930a940c
artemsky <mr.artemsky@gmail.com>
2020-02-16 23:26:00 +0200

    fix(services): add index description, fix margin

--- app/Http/Controllers/Client/BaseController.php
+++ app/Http/Controllers/Client/BaseController.php
@@ -18,7 +18,16 @@ class BaseController extends Controller
         $this->currentLocale = LaravelLocalization::getCurrentLocale() ?? 'uk';
         $setting = new Setting();
         $settings = $setting->first();
-        $services = Service::published()->get(['id', 'title_uk', 'title_en', 'image', 'url', 'url_target']);
+        $services = Service::published()->get([
+            'id',
+            'title_uk',
+            'title_en',
+            'image',
+            'url',
+            'url_target',
+            'description_uk',
+            'description_en'
+        ]);
         View::share('settings', $settings);
         View::share('services', $services);
         View::share('currentLocale', $this->currentLocale);
